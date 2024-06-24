b8b4106c476f0472a643731a1e12dcf2060e3b91
zeeshanakbarmalik <malikza.objects@gmail.com>
2024-06-24 20:33:25 +0500

    Created a function with two parameters.

--- day4_functions.dart
+++ day4_functions.dart
@@ -1,6 +1,13 @@
 void main() {
-  int number = 23;
-  print(number % 2);
+  //int number = 23;
+  //print(number % 2);
   // Assignment for leap year
-  print("leap year");
+  //print("leap year");
+  final greeting = greet(name: "Malik");
+  print(greeting);
+}
+
+String greet({required String name, int? age}) {
+  // We're using curly brackets inside the parameters for positional.
+  return "Hey, my full name is $name and I'm $age year's old.";
 }
