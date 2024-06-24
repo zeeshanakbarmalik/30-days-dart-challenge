02e653051a4aeab5cfe41a7aa15db2a1f079cf23
zeeshanakbarmalik <malikza.objects@gmail.com>
2024-06-24 21:00:00 +0500

    Pseudo code added at top.

--- day5_Lists_and_sets.dart
+++ day5_Lists_and_sets.dart
@@ -1,1 +1,13 @@
-void main() {}
+void main() {
+  /*
+   * Use .add() method to store a new value to the list index.
+   * Use .remove() method to remove any value from the list index.
+   * Use .removeLast() method to remove the same numbers in 
+   * different index/positions.
+   * <variable>.shuffle()
+   * Use .length in print statement with the variable name.
+   * Use .indexOf(5) in print statement with the variable name.
+   * To update or reassign a value from an index use variable name 
+   * with index number. (e.g. score[1] = value;)  
+   */
+}
